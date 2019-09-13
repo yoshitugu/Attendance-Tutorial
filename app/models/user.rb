0af06a7eb1f6e4b8c659431b9a1609bb0738aa4e
yoshitugu <yuki.38220512@gmail.com>
2019-09-13 01:50:04 +0000

    Finish add-basic-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,48 +1,51 @@
 class User < ApplicationRecord
   # 「remember_token」という仮想の属性を作成します。
   attr_accessor :remember_token
-  before_save { self.email = email.downcase } #保存される前に小文字として変換されてから保存される。
-  
-  validates :name, presence: true, length: { maximum: 50 }
+  before_save { self.email = email.downcase }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  validates :department, length: { in: 2..50 }, allow_blank: true
+  validates :department, length: { in: 2..50 }, allow_blank: true
+  validates :basic_time, presence: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 },allow_nil: true
-  
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
   # 渡された文字列のハッシュ値を返します。
   def User.digest(string)
     cost = 
-     if ActiveModel::SecurePassword.min_cost
-       BCrypt::Engine::MIN_COST
-     else
-       BCrypt::Engine.cost
-     end
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
     BCrypt::Password.create(string, cost: cost)
-  end  
-  
-  #ランダムなトークンを渡します。
+  end
+
+  # ランダムなトークンを返します。
   def User.new_token
     SecureRandom.urlsafe_base64
   end
-  
+
   # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end  
-  
-  
+  end
+
   # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
     # ダイジェストが存在しない場合はfalseを返して終了します。
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-  
+
   # ユーザーのログイン情報を破棄します。
   def forget
-   update_attribute(:remember_digest, nil)
+    update_attribute(:remember_digest, nil)
   end
 end
